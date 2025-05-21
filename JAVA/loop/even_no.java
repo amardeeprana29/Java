2d5c8f2b04542c75e7c0d700854b7725a187e6e3
Amardeep Rana <134470640+amardeeprana29@users.noreply.github.com>
2025-05-21 22:06:53 +0530

    Add files via upload

---
+++ JAVA/loop/even_no.java
@@ -0,0 +1,8 @@
+public class even_no {
+    public static void main(String[] args) {
+        System.out.println("A 'for' loop that prints out the first 10 even numbers: ");
+            for(int i=2; i<=20; i+=2){
+              System.out.println(i);   
+            }
+    }
+}
