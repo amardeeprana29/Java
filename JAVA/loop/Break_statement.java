53de78704ae1bf6ad11ccd446ed922f1e4dd40e6
Amardeep Rana <134470640+amardeeprana29@users.noreply.github.com>
2025-05-31 21:55:32 +0530

    Add files via upload

---
+++ JAVA/loop/Break_statement.java
@@ -0,0 +1,12 @@
+public class Break_statement {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+
+            if (i==5) {
+                break; // it is used to exit a loop early, before the loops condition is met.
+            } 
+            System.out.println(i);         
+        }
+    }
+    
+}
