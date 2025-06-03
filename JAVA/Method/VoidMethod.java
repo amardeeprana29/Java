028980ca372e3e6d4770441abd0370980ab18c9c
Amardeep Rana <134470640+amardeeprana29@users.noreply.github.com>
2025-06-03 23:38:36 +0530

    Add files via upload

---
+++ JAVA/Method/VoidMethod.java
@@ -0,0 +1,16 @@
+package Method;
+
+public class VoidMethod {
+    public static void main(String[] args) {
+        printhello();
+        printHello("Amardeep");
+    }
+
+    public static void printhello(){
+        System.out.println("Hello");
+    }
+    public static void printHello(String name){
+        System.out.println("Hello "+name+ "!");
+    }
+    
+}
